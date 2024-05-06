66366cff23737ef7af6878247bee4bacd6875b62
Túlio Chaves De Souza <tulio.souza@techne.com.br>
2024-05-06 11:37:17 +0000

    finalizando customização tabela grade

--- src/main/java/blockly/DadosMockados/TabelaHome.java
+++ src/main/java/blockly/DadosMockados/TabelaHome.java
@@ -14,7 +14,7 @@ public static final int TIMEOUT = 300;
 /**
  *
  * @author Túlio Chaves De Souza
- * @since 06/05/2024, 08:17:14
+ * @since 06/05/2024, 08:21:50
  *
  */
 public static Var DadosTabelaHome() throws Exception {
@@ -25,6 +25,26 @@ public static Var DadosTabelaHome() throws Exception {
    public Var call() throws Exception {
     resposta =
     cronapi.list.Operations.newList(
+    cronapi.map.Operations.createObjectMapWith(Var.valueOf("pedido",
+    Var.valueOf("1787")) , Var.valueOf("codCliente",
+    Var.valueOf("1231231421")) , Var.valueOf("valor",
+    Var.valueOf("R$ 63.000")) , Var.valueOf("previsao",
+    Var.valueOf("23/02/2023"))),
+    cronapi.map.Operations.createObjectMapWith(Var.valueOf("pedido",
+    Var.valueOf("1787")) , Var.valueOf("codCliente",
+    Var.valueOf("1231231421")) , Var.valueOf("valor",
+    Var.valueOf("R$ 63.000")) , Var.valueOf("previsao",
+    Var.valueOf("23/02/2023"))),
+    cronapi.map.Operations.createObjectMapWith(Var.valueOf("pedido",
+    Var.valueOf("1787")) , Var.valueOf("codCliente",
+    Var.valueOf("1231231421")) , Var.valueOf("valor",
+    Var.valueOf("R$ 63.000")) , Var.valueOf("previsao",
+    Var.valueOf("23/02/2023"))),
+    cronapi.map.Operations.createObjectMapWith(Var.valueOf("pedido",
+    Var.valueOf("1787")) , Var.valueOf("codCliente",
+    Var.valueOf("1231231421")) , Var.valueOf("valor",
+    Var.valueOf("R$ 63.000")) , Var.valueOf("previsao",
+    Var.valueOf("23/02/2023"))),
     cronapi.map.Operations.createObjectMapWith(Var.valueOf("pedido",
     Var.valueOf("1787")) , Var.valueOf("codCliente",
     Var.valueOf("1231231421")) , Var.valueOf("valor",
